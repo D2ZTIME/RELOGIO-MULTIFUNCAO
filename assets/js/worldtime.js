f0ea234fea8554b13b856538de1ae37f9c9bfa1a
D2Z TIME <CONTATO@D2Z-SHOP.COM.BR>
2025-04-11 16:16:02 -0300

    Create worldtime.js

---
+++ assets/js/worldtime.js
@@ -0,0 +1,16 @@
+// Implementação completa dos fusos horários
+class WorldTime {
+    constructor() {
+        this.timezones = {
+            'BRT': 'America/Sao_Paulo',
+            'EST': 'America/New_York',
+            'UTC': 'UTC',
+            'CET': 'Europe/Paris',
+            'JST': 'Asia/Tokyo'
+        };
+    }
+
+    getCurrentTimes() {
+        // Retorna horários em todos os fusos
+    }
+}
