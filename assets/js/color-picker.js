baef66f4568d8c4bb2fac8f5617928754046f2bc
D2Z TIME <CONTATO@D2Z-SHOP.COM.BR>
2025-04-11 16:16:53 -0300

    Create color-picker.js

---
+++ assets/js/color-picker.js
@@ -0,0 +1,14 @@
+// Seletor de cores personalizado
+class ColorPicker {
+    constructor() {
+        this.themes = {
+            'dark': { /* cores escuras */ },
+            'light': { /* cores claras */ },
+            'blue': { /* tema azul */ }
+        };
+    }
+
+    applyTheme(themeName) {
+        // Aplica o tema selecionado
+    }
+}
